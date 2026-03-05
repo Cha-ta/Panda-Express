1ce6474529984e70e699c5fb12f653b89f9356f6
Daniel <d.v1llas3nor@gmail.com>
2026-03-04 21:22:13 -0800

    switched to headless mode + added sandbox flags

--- script.py
+++ script.py
@@ -165,8 +165,12 @@ def is_finish_page(page):
 
 with sync_playwright() as p:
     browser = p.chromium.launch(
-        headless=False,
-        args=['--disable-blink-features=AutomationControlled']
+        headless=True,
+        args=[
+            '--disable-blink-features=AutomationControlled',
+            '--no-sandbox',
+            '--disable-dev-shm-usage'
+        ]
     )
     context = browser.new_context(
         user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
